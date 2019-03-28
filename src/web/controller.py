e7cb8b6018b97b3ae5c6e3de5af1c393a5a4d8e8
max.mehl <max.mehl@fsfe.org>
2019-03-28 12:53:48 +0100

    Revert "Fix form worker and web crash (#32)"
    This reverts commit 79e8687a2fff60e287138177232d086e74319ab3.

--- src/web/controller.py
+++ src/web/controller.py
@@ -15,9 +15,6 @@ def error_handler(func):
             abort(404, e.message)
         except redis.exceptions.ConnectionError as e:
             abort(500, 'Database connection failed')
-        except Exception as e:
-            print(e)
-            abort(500, str(e))
 
     return wrapper
 
