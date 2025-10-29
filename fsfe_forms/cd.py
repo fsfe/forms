d67b4894206f83d08b8b296f86a5410960f6495f
Filip Kobierski <fkobi@fsfe.org>
2025-10-29 15:25:24 +0100

    fsfe_forms/cd.py: use fstring
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- fsfe_forms/cd.py
+++ fsfe_forms/cd.py
@@ -61,7 +61,7 @@ def subscribe(config, params):
     # Determine command signature
     parts = ["command=persons.confirm", f"record_id={person_id}"]
     for key in sorted(confirm_params.keys()):
-        parts.append("{}={}".format(key, confirm_params[key]))
+        parts.append(f"{key}={confirm_params[key]}")
     parts.append(current_app.config["FSFE_CD_PASSPHRASE"])
     signature = sha256(";".join(parts).encode("UTF-8")).hexdigest()
 
