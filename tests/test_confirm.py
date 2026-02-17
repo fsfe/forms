95c25524fac12d7d4d4599229dc779b9f679a91b
Filip Kobierski <fkobi@fsfe.org>
2026-02-17 12:41:58 +0100

    tests/test_confirm.py: use HTTPStatus instead of magic numbers
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- tests/test_confirm.py
+++ tests/test_confirm.py
@@ -3,24 +3,26 @@
 This file is part of the FSFE Form Server.
 """
 
+from http import HTTPStatus
+
 
 def test_confirm_redeem_id(client, signed_up):
     """Confirm landing page with a valid ID"""
     response = client.get(path="/confirm", query_string={"id": signed_up})
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert bytes(f'<a href="/redeem?id={signed_up}"', "utf-8") in response.data
 
 
 def test_confirm_no_id(client):
     """Confirm landing page without an ID"""
     response = client.get(path="/confirm")
-    assert response.status_code == 422
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_CONTENT
 
 
 def test_confirm_bad_id(client):
     """Confirm landing page with a malformed ID"""
     response = client.get(path="/confirm", query_string={"id": "BAD-ID"})
-    assert response.status_code == 422
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_CONTENT
 
 
 def test_redeem(client, smtp_mock, redis_mock, file_mock, fsfe_cd_mock, signed_up):
@@ -28,7 +30,7 @@ def test_redeem(client, smtp_mock, redis_mock, file_mock, fsfe_cd_mock, signed_u
     name: str = "THE NAME"
 
     response = client.get(path="/redeem", query_string={"id": signed_up})
-    assert response.status_code == 302
+    assert response.status_code == HTTPStatus.FOUND
     assert (
         response.location == "https://fsfe.org/activities/ln/"
         "application-success.html"
@@ -56,17 +58,17 @@ def test_redeem_doubled(client, signed_up):
     """Redeem the same valid ID more than once"""
     client.get(path="/redeem", query_string={"id": signed_up})
     response = client.get(path="/redeem", query_string={"id": signed_up})
-    assert response.status_code == 404
+    assert response.status_code == HTTPStatus.NOT_FOUND
     assert b"No such pending confirmation ID" in response.data
 
 
 def test_redeem_no_id(client):
     """Redeem without an ID"""
     response = client.get(path="/redeem")
-    assert response.status_code == 422
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_CONTENT
 
 
 def test_redeem_bad_id(client):
     """Redeem with a malformed ID"""
     response = client.get(path="/redeem", query_string={"id": "BAD-ID"})
-    assert response.status_code == 422
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_CONTENT
