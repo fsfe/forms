d51b909b7c6f575776bcd89502b34cd05182c1a4
Filip Kobierski <fkobi@fsfe.org>
2026-02-17 12:28:28 +0100

    tests/__init__.py: clear the file
    Signed-off-by: Filip Kobierski <fkobi@fsfe.org>

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# =============================================================================
-# Functional tests for the FSFE Form Server
-# =============================================================================
-# This file is part of the FSFE Form Server.
-#
-# SPDX-FileCopyrightText: 2020 FSFE e.V. <contact@fsfe.org>
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
